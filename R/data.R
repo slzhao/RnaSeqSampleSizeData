5df6c39bab5bb2fca00e29a99e3e159be33d47c8
Shilin Zhao <zhaoshilin@gmail.com>
2014-11-23 19:14:34 -0600

    V0.99.2

--- R/data.R
+++ R/data.R
@@ -1,78 +1,92 @@
+#' @import edgeR
 #' @exportPattern "."
 #' @name TCGA_BLCA
 #' @title Read counts and dispersion distribution for genes in TCGA BLCA data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA BLCA data.
 #' @docType data
 #' @usage TCGA_BLCA
 NULL
 #' @name TCGA_BRCA
 #' @title Read counts and dispersion distribution for genes in TCGA BRCA data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA BRCA data.
 #' @docType data
 #' @usage TCGA_BRCA
 NULL
 #' @name TCGA_CESC
 #' @title Read counts and dispersion distribution for genes in TCGA CESC data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA CESC data.
 #' @docType data
 #' @usage TCGA_CESC
 NULL
 #' @name TCGA_COAD
 #' @title Read counts and dispersion distribution for genes in TCGA COAD data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA COAD data.
 #' @docType data
 #' @usage TCGA_COAD
 NULL
 #' @name TCGA_HNSC
 #' @title Read counts and dispersion distribution for genes in TCGA HNSC data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA HNSC data.
 #' @docType data
 #' @usage TCGA_HNSC
 NULL
 #' @name TCGA_KIRC
 #' @title Read counts and dispersion distribution for genes in TCGA KIRC data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA KIRC data.
 #' @docType data
 #' @usage TCGA_KIRC
 NULL
 #' @name TCGA_LGG
 #' @title Read counts and dispersion distribution for genes in TCGA LGG data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA LGG data.
 #' @docType data
 #' @usage TCGA_LGG
 NULL
 #' @name TCGA_LUAD
 #' @title Read counts and dispersion distribution for genes in TCGA LUAD data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA LUAD data.
 #' @docType data
 #' @usage TCGA_LUAD
 NULL
 #' @name TCGA_LUSC
 #' @title Read counts and dispersion distribution for genes in TCGA LUSC data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA LUSC data.
 #' @docType data
 #' @usage TCGA_LUSC
 NULL
 #' @name TCGA_PRAD
 #' @title Read counts and dispersion distribution for genes in TCGA PRAD data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA PRAD data.
 #' @docType data
 #' @usage TCGA_PRAD
 NULL
 #' @name TCGA_READ
 #' @title Read counts and dispersion distribution for genes in TCGA READ data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA READ data.
 #' @docType data
 #' @usage TCGA_READ
 NULL
 #' @name TCGA_THCA
 #' @title Read counts and dispersion distribution for genes in TCGA THCA data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA THCA data.
 #' @docType data
 #' @usage TCGA_THCA
 NULL
 #' @name TCGA_UCEC
 #' @title Read counts and dispersion distribution for genes in TCGA UCEC data.
+#' @return A class 'DGEList' from edgeR package.
 #' @description Read counts and dispersion distribution for genes in TCGA UCEC data.
 #' @docType data
 #' @usage TCGA_UCEC
